495b795924c7ff7d6eb3299c8d1520c37494ad71
Mohit Kumar <156556303+MohitKumar509@users.noreply.github.com>
2026-09-23 22:07:55 +0530

    Time: 55 ms (41.76%), Space: 70.9 MB (13.16%) - LeetHub

--- Java/0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ Java/0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,20 +1,37 @@
 class Solution {
     public int totalFruit(int[] fruits) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0;
-        int max = 0;
-        for (int right = 0; right < fruits.length; right++) {
 
-            map.put(fruits[right], map.getOrDefault(fruits[right], 0) + 1);
-            while (map.size() > 2) {
-                map.put(fruits[left], map.get(fruits[left]) - 1);
-                if (map.get(fruits[left]) == 0) {
-                    map.remove(fruits[left]);
+        Map<Integer,Integer> map=new HashMap<>();
+        int i=0;
+        int j=0;
+        int max=0;
+        while(j<fruits.length){
+            map.put(fruits[j], map.getOrDefault(fruits[j],0)+1);
+
+            while(map.size()>2){
+                map.put(fruits[i],map.get(fruits[i])-1);
+                if(map.get(fruits[i])==0){
+                    map.remove(fruits[i]);
                 }
-                left++;
+                i=i+1;
             }
-            max = Math.max(max, right - left + 1);
+            max=Math.max(max,j-i+1);
+            j=j+1;
         }
         return max;
+        // int max=0;
+        // for(int i=0;i<fruits.length;i++){
+        //     Set<Integer> set=new HashSet<>();
+        //     for(int j=i;j<fruits.length;j++){
+        //         set.add(fruits[j]);
+
+        //         if(set.size()>2){
+        //             break;
+        //         }
+        //         max=Math.max(max,j-i+1);
+        //     }
+        // }
+        // return max;
+        
     }
 }
